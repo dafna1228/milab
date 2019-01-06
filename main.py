9bc7ca26d597fbf9bc37b5a4705967c4f22a3d2c
Yotam <YotamNHL@Gmail.com>
2019-01-06 21:12:20 +0200

    sdfsdfA

--- main.py
+++ main.py
@@ -4,35 +4,50 @@ import pygame
 from playsound import playsound
 from time import sleep
 
+#Init Pygame to play different soundbits
 pygame.mixer.init()
-#playsound('song.mp3')
+
+#Start the playback
+playsound("Audio/PLAYBACK.mp3", 0)
 
 if __name__ == "__main__":
-    Car = 'Sound_Car.mp3'
-    Nature = 'Sound_Nature.mp3'
-    Sign = 'Sound_Sign.mp3'
-    imageFile = 'footage.jpg'
-    client = boto3.client('rekognition', 'eu-west-1', aws_access_key_id='ASIA4QPZ4Y5UK5EWKOOH', aws_secret_access_key='n/pyrQlDOCdpIUYI+YyeQkzXfZWJCLMzsqNIQa9A', aws_session_token="FQoGZXIvYXdzEOv//////////wEaDMmM4ZNTwbB7MPv2gyLrAguyXZggqT6YOQaHa4m1/GMI+sl0ua+uo/pRG/mYYg0Ye7D3DHOYpAldPKQzq5appNG67EjM3FZuYhYTo/9Sphy/UWuXua4qc8Eazgwz3nHUEPgPqrOf8nraRy/AidOu2dnjiIq7yEPklxzuagki8CIX4vdCfGvsj1JThNsh0NAp7TrFJSsr+YySt9gXN1oLklknpyzganm2gLHomZLpIEOJ0ZQ1O83spcGKR4wbGIMbm08Xwq7cLxoBN8M6FHX81udJO3zH3eVmZvDy5BoPsA9Q6in0mf0TRazPmSzn3wHJz8ltfbh35vok+As6fRUtEZeANGV2gdixh2/e1LZO9iWsdz1lp65JiCgjZjI+M/QR4SBSczJm/RwUIyX8i1su38JAg6a3VYzfy6dP2k+XgAmt8kstjX1uYtH3NjVLQZgoKStoqF1QvZSDvJaZMr8c1qC1o55Ugs7R1DSbvfhmWfev+KgzaF74niR4eyiLncfhBQ==")
+    #connect to AWS image recognition client
+    client = boto3.client('rekognition', 'eu-west-1', aws_access_key_id='???',
+                          aws_secret_access_key='???',
+                          aws_session_token="???")
+    #soundbits files:
+    Car = 'Audio/Sound_Car.mp3'
+    Nature = 'Audio/Sound_Nature.mp3'
+    Sign = 'Audio/Sound_Sign.mp3'
+
+    #Runs forever, if it finds a new image in the folder, it uses the AWS-IR service to scan objects in the photo
     while True:
-            for filename in os.listdir(os.getcwd() + '/images'):
-                with open(imageFile, 'rb') as image:
-                    response = client.detect_labels(Image={'Bytes': image.read()})
-                print(response)
-                lst = response['Labels']
-                results = {}
-                for i in lst:
-                    results[i['Name']] = i['Confidence']
+        sleep(0.5)
+        for filename in os.listdir(os.getcwd() + '\\Images'):
+            with open(os.getcwd() + "\\Images\\" + filename, 'rb') as image:
+                 #create a JSON dict in "response" that holds the objects in the image
+                 response = client.detect_labels(Image={'Bytes': image.read()})
+            lst = response['Labels']
+            results = {}
+            #filters and orders the JSON dict so it would only hold the object and the confidence value of that object
+            for i in lst:
+                results[i['Name']] = i['Confidence']
+            if 'Sign' in results:
+                if results.get('Sign') > 50:
+                    pygame.mixer.music.load(Sign)
+                    pygame.mixer.music.play()
+                    print("BlaBlaBla...")
+            elif 'Nature' in results:
+                if results.get('Nature') > 50:
+                    pygame.mixer.music.load(Nature)
+                    pygame.mixer.music.play()
+                    print("Sounds of nature!")
+            elif 'Car' in results:
                 if results.get('Car') > 50:
                     pygame.mixer.music.load(Car)
                     pygame.mixer.music.play()
-                elif results.get('Nature') > 50:
-                    pygame.mixer.music.load(Nature)
-                    pygame.mixer.music.play()
-                elif results.get('Words') > 50:
-                    pygame.mixer.music.load(Sign)
-                    pygame.mixer.music.play()
-                os.remove(os.getcwd() + '/images/' + filename)
-                results = {}
+                    print("Beep Beep!!!")
+            os.remove(os.getcwd() + '/images/' + filename)
+            results = {}
             for label in response['Labels']:
                 print(label['Name'] + ' : ' + str(label['Confidence']))
-    print('Done...')
